c8b806ceb57a067347600622454e664f603f2839
Lovnish Verma <princelv84@gmail.com>
2025-05-30 12:15:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 # Streamlit application example
 
 # Title
-st.title("Hello, AIML!")
+st.title("Hello, AIML wale students!")
 
 # Simple text
 st.write("Welcome to your first Streamlit app.")
